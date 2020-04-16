0a681fd685b26dca20958c3829987a2d9f15ec4b
[enzo.mandine] <[enzo.mandine@laplateforme.io]>
2020-04-16 17:42:46 +0200

    err

--- connexion.php
+++ connexion.php
@@ -1,36 +1,39 @@
 <?php
 session_start();
 include 'header.php';
-if (isset($_POST["envoie"])) {
-	$conn = mysqli_connect("localhost", "root", "", "memory");
-	$request = "SELECT * FROM utilisateurs WHERE login ='" . htmlspecialchars($_POST["login"]) . "'";
-	$sql = mysqli_query($conn, $request);
-	$row = mysqli_fetch_assoc($sql);
-	if ($sql == TRUE) {
-		if (password_verify($_POST["mdp"], $row["password"])) {
-			$_SESSION["id"] = $row["id"];
-			$_SESSION["login"] = $row["login"];
-			$_SESSION["rang"] = $row["rang"];
-			$_SESSION["connected"] = true;
-			header("location:index.php");
-		} else {
-			echo "Mauvais password";
-		}
-	} else {
-		echo "login inconnu";
-	}
-}
 ?>
 <div class='connexionbg center'>
-<a href="index.php"><img src="images/back.png"></a>
-		<form class="form connexionelement" action="" method="post">
-			<label for="login">Votre pseudo</label></br><br>
-			<input class="type_texte" type="text" name="login" /><br></br>
-			<label for="mdp">Votre mot de passe</label></br><br>
-			<input class="type_texte" type="password" name="mdp" /></br>
-			<p>pas encore de compte ?<a href='inscription.php'><br>inscrivez vous</a></p>
-			<input id="submit_connexion" class="submit_btn" type="submit" value="Se connecter" name="envoie" />
-		</form>
+	<a href="index.php"><img src="images/back.png"></a>
+	<form class="form connexionelement" action="" method="post">
+		<label for="login">Votre pseudo</label></br><br>
+		<input class="type_texte" type="text" name="login" /><br></br>
+		<label for="mdp">Votre mot de passe</label></br><br>
+		<input class="type_texte" type="password" name="mdp" /></br>
+		<p>pas encore de compte ?<a class="loghref" href='inscription.php'><br>inscrivez vous</a></p>
+		<input id="submit_connexion" class="submit_btn" type="submit" value="Se connecter" name="envoie" />
+		<br>
+		<?php
+		if (isset($_POST["envoie"])) {
+			$conn = mysqli_connect("localhost", "root", "", "memory");
+			$request = "SELECT * FROM utilisateurs WHERE login ='" . htmlspecialchars($_POST["login"]) . "'";
+			$sql = mysqli_query($conn, $request);
+			$row = mysqli_fetch_assoc($sql);
+			if ($sql == TRUE) {
+				if (password_verify($_POST["mdp"], $row["password"])) {
+					$_SESSION["id"] = $row["id"];
+					$_SESSION["login"] = $row["login"];
+					$_SESSION["rang"] = $row["rang"];
+					$_SESSION["connected"] = true;
+					header("location:index.php");
+				} else {
+					echo "<p class='err'>Mauvais password</p>";
+				}
+			} else {
+				echo "<p class='err'>login inconnu</p>";
+			}
+		}
+		?>
+	</form>
 </div>
 
 <?php
--- inscription.php
+++ inscription.php
@@ -2,32 +2,6 @@
 session_start();
 include 'header.php';
 $_SESSION["validation"] = true;
-
-if (isset($_POST["envoie"])) {
-	if ($_POST["mdp"] == $_POST['remdp']) {
-		echo "test";
-		$conn     = mysqli_connect("localhost", "root", "", "memory");
-		$request  = "SELECT login FROM utilisateurs";
-		$query    = mysqli_query($conn, $request);
-		$response = mysqli_fetch_all($query);
-
-		$count = 0;
-		while ($count < count($response)) {
-			if ($response[$count][0] == $_POST["login"]) {
-				$_SESSION["validator"] = false;
-				header("location:inscription.php");
-			}
-			$count++;
-		}
-
-		if ($_SESSION["validation"]) {
-			$request = "INSERT INTO utilisateurs (`id`,`login`,`password`) VALUES (NULL,'" . $_POST["login"] . "','" . password_hash($_POST["mdp"], PASSWORD_BCRYPT) . "');";
-			mysqli_query($conn, $request);
-			header("location:connexion.php");
-		}
-	}
-}
-
 ?>
 <div class='inscriptionbg center'>
 	<a href="index.php"><img src="images/back.png"></a>
@@ -41,6 +15,33 @@ if (isset($_POST["envoie"])) {
 		<label for="remdp">Confirmez votre mot de passe</label><br>
 		<input class="type_texte" type="password" name="remdp" />
 		<input id="submit_inscription" class="submit_btn" type="submit" name="envoie" value="Se connecter" />
+		<?php
+		if (isset($_POST["envoie"])) {
+			if ($_POST["mdp"] == $_POST['remdp']) {
+				$conn     = mysqli_connect("localhost", "root", "", "memory");
+				$request  = "SELECT login FROM utilisateurs";
+				$query    = mysqli_query($conn, $request);
+				$response = mysqli_fetch_all($query);
+
+				$count = 0;
+				while ($count < count($response)) {
+					if ($response[$count][0] == $_POST["login"]) {
+						$_SESSION["validator"] = false;
+						header("location:inscription.php");
+					}
+					$count++;
+				}
+
+				if ($_SESSION["validation"]) {
+					$request = "INSERT INTO utilisateurs (`id`,`login`,`password`) VALUES (NULL,'" . $_POST["login"] . "','" . password_hash($_POST["mdp"], PASSWORD_BCRYPT) . "');";
+					mysqli_query($conn, $request);
+					header("location:connexion.php");
+				}
+			} else {
+				echo "<p class='err'>Password differents</p>";
+			}
+		}
+		?>
 	</form>
 </div>
 <?php
